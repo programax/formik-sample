08aebe449f6576b8d69892f43cb57da5305f47a2
Leonso <leonziyo@gmail.com>
2018-12-09 15:05:38 -0800

    inital

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { withFormik, Field, ErrorMessage, Form } from 'formik';
 
-class App extends Component {
-  render() {
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function MyForm(props) {
+    const {
+        isSubmitting,
+        isValid,
+    } = props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Form>
+            <div className="row">
+                Email:
+                <Field name="email" type="email" className="input" />
+                <ErrorMessage name="email">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+
+            <div className="row">
+                Password:
+                <Field name="password" type="password" className="input" />
+                <ErrorMessage name="password">
+                    {message => <div className="error">{message}</div>}
+                </ErrorMessage>
+            </div>
+
+            <div className="row">
+                <button
+                    type="submit"
+                    className={`submit ${isSubmitting || !isValid ? 'disabled' : ''}`}
+                    disabled={isSubmitting || !isValid}
+                >
+                    Submit
+                </button>
+            </div>
+        </Form>
     );
-  }
 }
 
-export default App;
+export default withFormik({
+    mapPropsToValues(props) {
+        return {
+            email: props.defaultEmail,
+            password: '',
+        };
+    },
+
+    async validate(values) {
+        const errors = {};
+
+        if (!values.password) {
+            errors.password = 'Password is required';
+        } else if (values.password.length < 8) {
+            errors.password = 'Password must be at least 8 characters';
+        }
+
+        await sleep(5000);
+
+        if (Object.keys(errors).length) {
+            throw errors;
+        }
+    },
+
+    handleSubmit(values, formikBag) {
+        formikBag.setSubmitting(false);
+        console.log(values);
+    },
+})(MyForm);
